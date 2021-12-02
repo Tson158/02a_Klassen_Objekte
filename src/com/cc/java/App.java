859e3e7f23f5c9b77acb8480af0d779c59016c38
Kakarott <Sonntag_thomas_86@web.de>
2021-12-02 11:11:21 +0100

    wertzuweisungvon aussen / innen

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -4,10 +4,29 @@ public class App {
     
     public static void main(String[] args) {
 
-        Cat cat = new Cat(); // Was erzeugen wir hier eigentlich??
-        System.out.println("Blick von aussen" + cat);
-    
-        cat.tellYourAddress();
+        Cat cat1 = new Cat(); // Was erzeugen wir hier eigentlich?? --> Kalle zu Objekt/instanz
+        System.out.println("Blick von aussen " + cat1);
+        cat1.name = "Thomas";
+        cat1.furColor = "White";
+        cat1.age = 35;
+        cat1.tellYourAddress();
+        System.out.println(cat1.tellYourName());
+        System.out.println(cat1.furColor);
+        System.out.println(cat1.age);
+
+
+        System.out.println("**********************************");
+
+        Cat cat2 = new Cat(); // Was erzeugen wir hier eigentlich?? --> Kalle zu Objekt/instanz
+        System.out.println("Blick von aussen " + cat2);
+        cat2.name = "Alonso";
+        cat2.furColor = "Black";
+        cat2.age = 39;
+        cat2.tellYourAddress();
+        System.out.println(cat2.tellYourName());
+        System.out.println(cat2.furColor);
+        System.out.println(cat2.age);
+
     }
 
 }
--- src/com/cc/java/Cat.java
+++ src/com/cc/java/Cat.java
@@ -2,9 +2,18 @@ package com.cc.java;
 
 public class Cat {
 
+    public String name;
+    public String furColor;
+    public int age;
+
+
     public void tellYourAddress(){
-        System.out.println("Blick von innen" + this);
+        System.out.println("Blick von innen " + this);
     }
 
 
+    public String tellYourName(){
+        return this.name;
+    }
+    
 }
